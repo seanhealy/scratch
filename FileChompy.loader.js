b5ba17b1de1b5a129dc0fed8eb6d3789b1869945
Sean Healy <s@xib.ca>
2025-11-07 14:55:34 -0700

    chore: update loading

---
+++ FileChompy.loader.js
@@ -0,0 +1,9 @@
+(() => {
+	const script = document.createElement("script");
+	script.src = "https://cdn.jsdelivr.net/gh/seanhealy/scratch/FileChompy.js";
+
+	script.onload = () => console.log("FileChompy loaded successfully!");
+	script.onerror = () => console.error("Error loading FileChompy");
+
+	document.head.appendChild(script);
+})();
